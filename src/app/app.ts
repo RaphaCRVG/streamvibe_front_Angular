7ede1f98ce1b872e679e16c4b4b516d7f562d2cf
Raphael Oliveira <oliveira.raphael@aluno.senac.rio>
2025-07-10 19:52:50 -0300

    Subindo alterações dia 10/07/2025 - pré intervalo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './componentes/header/header';
 import { Filme } from './componentes/filme/filme';
 import { Footer } from './componentes/footer/footer';
